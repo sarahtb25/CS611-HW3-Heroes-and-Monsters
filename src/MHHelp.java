fcce477b3961a2ececf434d4a7e092a41d8fa772
Sarah Taaher Bonna <sbonna@bu.edu>
2022-11-01 19:24:33 -0400

    Added Help Interface. Added empty MHHelp class

---
+++ src/MHHelp.java
@@ -0,0 +1,12 @@
+// Help for Monsters and Heroes
+public class MHHelp implements Help {
+    @Override
+    public void printIntroduction() {
+
+    }
+
+    @Override
+    public void printHelp() {
+
+    }
+}
