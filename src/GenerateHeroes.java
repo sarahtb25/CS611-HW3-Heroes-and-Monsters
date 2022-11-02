5f15e65988fa73d99907ec8ea6ad74d176897123
Sarah Taaher Bonna <sbonna@bu.edu>
2022-11-01 21:50:49 -0400

    Ensured no monsters and heroes are repeated each time they are generated

--- src/GenerateHeroes.java
+++ src/GenerateHeroes.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // This class generates heroes according to the number of heroes wanted by player
 public class GenerateHeroes {
     public static final WarriorFactory wf = new WarriorFactory();
@@ -13,6 +16,9 @@ public class GenerateHeroes {
     public Heroes getHeroes() {
         Heroes heroes = new Heroes();
         Hero hero = new Hero();
+        List<Integer> warriorIndexesAdded = new ArrayList<>();
+        List<Integer> sorcererIndexesAdded = new ArrayList<>();
+        List<Integer> paladinIndexesAdded = new ArrayList<>();
 
         for (int i = 0; i < numberOfHeroes; i++) {
             int index = (int) (Math.random() * numberOfTypesOfHeroes);
@@ -20,12 +26,30 @@ public class GenerateHeroes {
 
             if (typeOfHero.equals("Warrior")) {
                 int indexOfWarrior = (int) (Math.random() * wf.getNumberOfHeroes());
+
+                while(warriorIndexesAdded.contains(indexOfWarrior)) {
+                    indexOfWarrior = (int) (Math.random() * wf.getNumberOfHeroes());
+                }
+
+                warriorIndexesAdded.add(indexOfWarrior);
                 hero = wf.getAHero(indexOfWarrior);
             } else if (typeOfHero.equals("Sorcerer")) {
                 int indexOfSorcerer = (int) (Math.random() * sf.getNumberOfHeroes());
+
+                while(sorcererIndexesAdded.contains(indexOfSorcerer)) {
+                    indexOfSorcerer = (int) (Math.random() * sf.getNumberOfHeroes());
+                }
+
+                sorcererIndexesAdded.add(indexOfSorcerer);
                 hero = sf.getAHero(indexOfSorcerer);
             } else if (typeOfHero.equals("Paladin")) {
                 int indexOfPaladin = (int) (Math.random() * pf.getNumberOfHeroes());
+
+                while(paladinIndexesAdded.contains(indexOfPaladin)) {
+                    indexOfPaladin = (int) (Math.random() * pf.getNumberOfHeroes());
+                }
+
+                paladinIndexesAdded.add(indexOfPaladin);
                 hero = pf.getAHero(indexOfPaladin);
             }
 
--- src/GenerateMonsters.java
+++ src/GenerateMonsters.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // This class generates monsters as needed for a cell
 public class GenerateMonsters {
     public static final DragonFactory df = new DragonFactory();
@@ -13,6 +16,9 @@ public class GenerateMonsters {
     public Monsters getMonsters() {
         Monsters monsters = new Monsters();
         Monster monster = new Monster();
+        List<Integer> dragonIndexesAdded = new ArrayList<>();
+        List<Integer> exoskeletonIndexesAdded = new ArrayList<>();
+        List<Integer> spiritIndexesAdded = new ArrayList<>();
 
         for (int i = 0; i < numberOfHeroes; i++) {
             int index = (int) (Math.random() * numberOfTypesOfMonsters);
@@ -20,12 +26,30 @@ public class GenerateMonsters {
 
             if (typeOfMonster.equals("Dragon")) {
                 int indexOfDragon = (int) (Math.random() * df.getNumberOfMonsters());
+
+                while(dragonIndexesAdded.contains(indexOfDragon)) {
+                    indexOfDragon = (int) (Math.random() * df.getNumberOfMonsters());
+                }
+
+                dragonIndexesAdded.add(indexOfDragon);
                 monster = df.getAMonster(indexOfDragon);
             } else if (typeOfMonster.equals("Exoskeleton")) {
                 int indexOfExoskeleton = (int) (Math.random() * ef.getNumberOfMonsters());
+
+                while(exoskeletonIndexesAdded.contains(indexOfExoskeleton)) {
+                    indexOfExoskeleton = (int) (Math.random() * ef.getNumberOfMonsters());
+                }
+
+                exoskeletonIndexesAdded.add(indexOfExoskeleton);
                 monster = ef.getAMonster(indexOfExoskeleton);
             } else if (typeOfMonster.equals("Spirit")) {
                 int indexOfSpirit = (int) (Math.random() * sf.getNumberOfMonsters());
+
+                while(spiritIndexesAdded.contains(indexOfSpirit)) {
+                    indexOfSpirit = (int) (Math.random() * sf.getNumberOfMonsters());
+                }
+
+                dragonIndexesAdded.add(indexOfSpirit);
                 monster = sf.getAMonster(indexOfSpirit);
             }
 
